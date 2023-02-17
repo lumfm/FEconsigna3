16bb439b738470c954ca57d4832d6ee88956bbb6
Luis Molina <luismi_rocks@hotmail.com>
2023-02-16 21:01:31 -0500

    segundo comit, final del ejercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import isOdd from 'is-odd'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(isOdd(1));
   return (
     <div className="App">
       
